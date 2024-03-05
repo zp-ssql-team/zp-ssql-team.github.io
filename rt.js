cbab1a3750f37fe61a651ab5eb573c775d962287
zwr <weiren@badambiz.com>
2024-03-05 18:02:52 +0800

    reportError

--- rt.js
+++ rt.js
@@ -1,260 +1,276 @@
 wrapGetSawa("/api/sys/webrtc_get_info/").then((r) => {
-  var data = r.data;
-  var uuid = data.uuid;
-  var carrier = data.carrier;
-  console.log(uuid, carrier);
+    var data = r.data;
+    var uuid = data.uuid;
+    var carrier = data.carrier;
+    console.log(uuid, carrier);
 
-  // ("98D02113DB7B6C530DE705CC5757AF229D77A091");  Android uuid 示例
-  // ("461F71B8-CA4C-41E7-84D9-D0963DF26EA0");  iOS uuid 示例
-  const grayscalePercentage = 5; // 5%比例
-  const result = getGrayScaleUuid(uuid, grayscalePercentage);
+    // ("98D02113DB7B6C530DE705CC5757AF229D77A091");  Android uuid 示例
+    // ("461F71B8-CA4C-41E7-84D9-D0963DF26EA0");  iOS uuid 示例
+    const grayscalePercentage = 5; // 5%比例
+    const result = getGrayScaleUuid(uuid, grayscalePercentage);
 
-  if (result) {
-    testRTC(uuid, carrier);
-  }
+    if (result) {
+        testRTC(uuid, carrier);
+    }
 });
 
 if (!window.JavaBridgeObj) {
-  // test(); // 本地测试
+    // test(); // 本地测试
 }
 
 function uuidToInteger(uuid) {
-  const integer = parseInt(uuid.replace(/-/g, ""), 16);
-  return integer;
+    const integer = parseInt(uuid.replace(/-/g, ""), 16);
+    return integer;
 }
 
 function getGrayScaleUuid(uuid, percentage) {
-  uuid = uuid.replace(/-/g, "");
-  var len = uuid.length;
-  var totalValues = Math.pow(16, len);
-  var integerUuid = uuidToInteger(uuid);
-  var grayScaleValue = percentage / 100;
-  var threshold = integerUuid / totalValues;
+    uuid = uuid.replace(/-/g, "");
+    var len = uuid.length;
+    var totalValues = Math.pow(16, len);
+    var integerUuid = uuidToInteger(uuid);
+    var grayScaleValue = percentage / 100;
+    var threshold = integerUuid / totalValues;
 
-  console.log("灰度", threshold, grayScaleValue);
+    console.log("灰度", threshold, grayScaleValue);
 
-  return threshold <= grayScaleValue;
+    return threshold <= grayScaleValue;
 }
 
 function testRTC(uuid, carrier) {
-  UUID = uuid || Math.floor(Math.random() * 0xffffff).toString(16);
-  OPERATOR = carrier || "cmcc";
+    UUID = uuid || Math.floor(Math.random() * 0xffffff).toString(16);
+    OPERATOR = carrier || "cmcc";
 
-  function printCallback(name) {
-    return function () {
-      console.log(`[printCallback] [${name}]:`, arguments);
-    };
-  }
-
-  configuration = {
-    iceServers: [
-      {
-        urls: "stun:stun.l.google.com:19302", // 使用谷歌的stun服务
-      },
-    ],
-  };
-  pc = null;
-  sendChannel = null;
-
-  l = function (...args) {
-    console.log(...args);
-  };
-
-  e = function (...args) {
-    console.error(...args);
-  };
-
-  ws = new WebSocket(
-    `wss://sawa-ecs.badambiz.com/z1h/api/webrtc-test-hub?operator=${OPERATOR}&uuid=${UUID}`
-  );
-
-  ws.onopen = () => {
-    l("ws open");
-  };
-  ws.onclose = (err) => {
-    e("ws close", err);
-  };
-  ws.onerror = (err) => {
-    e("ws error", err);
-  };
-
-  function sendMessage(message) {
-    ws.send("1" + JSON.stringify(message));
-  }
-
-  function localDescCreated(desc) {
-    pc.setLocalDescription(
-      desc,
-      function () {
-        sendMessage({
-          action: "chat",
-          task,
-          another,
-          content: { sdp: pc.localDescription },
-        });
-      },
-      printCallback("setLocalDescription")
-    );
-  }
-
-  function reportResult(content) {
-    // return 111
-    sendMessage({
-      action: "report",
-      another,
-      task,
-      content,
-    });
-    l("reportResult", content);
-  }
-
-  ws.onmessage = function (evt) {
-    try {
-      switch (evt.data.substring(0, 1)) {
-        case "1":
-          var { action, task, another, role, content } = JSON.parse(
-            evt.data.substring(1)
-          );
-          switch (action) {
-            case "arrange":
-              try {
-                if (sendChannel) sendChannel.close();
-                if (pc) pc.close();
-              } catch (err) {
-                e("arrange close failed:", err);
-              }
-              window.task = task;
-              window.another = another;
-              window.isOfferer = role == "offer";
-              startWebRTC(role == "offer");
-              break;
-            case "chat":
-              if (content.sdp) {
-                // if (done)
-                //     return
-                // done = true
-                // 设置远程sdp, 在offer 或者 answer后
-                // console.log("message.sdp:", message.sdp)
-                pc.setRemoteDescription(
-                  new RTCSessionDescription(content.sdp),
-                  function () {
-                    // 当收到offer 后就接听
-                    if (pc.remoteDescription.type === "offer") {
-                      pc.createAnswer()
-                        .then(localDescCreated)
-                        .catch(printCallback("createAnswer error"));
-                    }
-                  },
-                  printCallback("setRemoteDescription")
-                );
-              } else if (content.candidate) {
-                // 增加新的 ICE canidatet 到本地的链接中
-                pc.addIceCandidate(
-                  new RTCIceCandidate(content.candidate),
-                  printCallback("addIceCandidate succ"),
-                  printCallback("addIceCandidate error")
-                );
-              }
-              break;
-          }
-          break;
-      }
-    } catch (err) {
-      e("ws message", err);
+    function toStr(a) {
+        try {
+            if (typeof(a) === 'object')
+                return JSON.stringify(a)
+        } catch (er) {}
+        return "" + a
     }
-  };
-  setInterval(function () {
-    ws.send("0");
-  }, 30000);
 
-  function startWebRTC(isOfferer) {
-    l("startWebRTC isOfferer = ", isOfferer);
-    var startTime = new Date().getTime();
-    var openTime = 0;
-    pc = new RTCPeerConnection(configuration);
-    sendChannel = pc.createDataChannel("sendDataChannel");
-    sendChannel.onopen = printCallback("sendChannel.onopen");
-    sendChannel.onclose = printCallback("sendChannel.onclose");
-    sendChannel.onerror = printCallback("sendChannel.onerror");
-    sendChannel.onmessage = printCallback("sendChannel.onmessage");
-    var isFirstReceive = false;
+    function printCallback(name) {
+        return function(a1, a2, a3) {
+            if (name.endsWith(".onerror")) {
+                a1 = toStr(a1)
+                a2 = toStr(a2)
+                a3 = toStr(a3)
+                reportResult({
+                    result: "error",
+                    scene: name,
+                    msg: { a1, a2, a3 },
+                })
+            }
+            console.log(`[printCallback] [${name}]:`, arguments);
+        };
+    }
 
-    var t = task;
-    setTimeout(function () {
-      if (t === window.task && !isFirstReceive)
-        reportResult({
-          result: "Timeout",
-          // cost: (new Date().getTime() - startTime()),
+    configuration = {
+        iceServers: [{
+            urls: "stun:stun.l.google.com:19302", // 使用谷歌的stun服务
+        }, ],
+    };
+    pc = null;
+    sendChannel = null;
+
+    l = function(...args) {
+        console.log(...args);
+    };
+
+    e = function(...args) {
+        console.error(...args);
+    };
+
+    ws = new WebSocket(
+        `wss://sawa-ecs.badambiz.com/z1h/api/webrtc-test-hub?operator=${OPERATOR}&uuid=${UUID}`
+    );
+
+    ws.onopen = () => {
+        l("ws open");
+    };
+    ws.onclose = (err) => {
+        e("ws close", err);
+    };
+    ws.onerror = (err) => {
+        e("ws error", err);
+    };
+
+    function sendMessage(message) {
+        ws.send("1" + JSON.stringify(message));
+    }
+
+    function localDescCreated(desc) {
+        pc.setLocalDescription(
+            desc,
+            function() {
+                sendMessage({
+                    action: "chat",
+                    task,
+                    another,
+                    content: { sdp: pc.localDescription },
+                });
+            },
+            printCallback("setLocalDescription")
+        );
+    }
+
+    function reportResult(content) {
+        // return 111
+        sendMessage({
+            action: "report",
+            another,
+            task,
+            content,
         });
+        l("reportResult", content);
+    }
+
+    ws.onmessage = function(evt) {
+        try {
+            switch (evt.data.substring(0, 1)) {
+                case "1":
+                    var { action, task, another, role, content } = JSON.parse(
+                        evt.data.substring(1)
+                    );
+                    switch (action) {
+                        case "arrange":
+                            try {
+                                if (sendChannel) sendChannel.close();
+                                if (pc) pc.close();
+                            } catch (err) {
+                                e("arrange close failed:", err);
+                            }
+                            window.task = task;
+                            window.another = another;
+                            window.isOfferer = role == "offer";
+                            startWebRTC(role == "offer");
+                            break;
+                        case "chat":
+                            if (content.sdp) {
+                                // if (done)
+                                //     return
+                                // done = true
+                                // 设置远程sdp, 在offer 或者 answer后
+                                // console.log("message.sdp:", message.sdp)
+                                pc.setRemoteDescription(
+                                    new RTCSessionDescription(content.sdp),
+                                    function() {
+                                        // 当收到offer 后就接听
+                                        if (pc.remoteDescription.type === "offer") {
+                                            pc.createAnswer()
+                                                .then(localDescCreated)
+                                                .catch(printCallback("createAnswer error"));
+                                        }
+                                    },
+                                    printCallback("setRemoteDescription")
+                                );
+                            } else if (content.candidate) {
+                                // 增加新的 ICE canidatet 到本地的链接中
+                                pc.addIceCandidate(
+                                    new RTCIceCandidate(content.candidate),
+                                    printCallback("addIceCandidate succ"),
+                                    printCallback("addIceCandidate error")
+                                );
+                            }
+                            break;
+                    }
+                    break;
+            }
+        } catch (err) {
+            e("ws message", err);
+        }
+    };
+    setInterval(function() {
+        ws.send("0");
     }, 30000);
 
-    pc.ondatachannel = (e) => {
-      console.log("[pc.ondatachannel]", arguments);
-      window.dc = e.channel;
-      var randNum = parseInt(Math.random() * 0xffffff);
-      dc.onopen = (ev) => {
-        openTime = new Date().getTime();
-        // printCallback('dc.onopen')
-        if (isOfferer) sendChannel.send("" + randNum);
-        // reportResult({
-        //     result: "answererReceive",
-        //     afterStart: curTime - startTime,
-        //     afterOpen: curTime - openTime,
-        // })
-      };
-      dc.onmessage = function (event) {
-        if (isFirstReceive) return;
-        isFirstReceive = true;
-        l("dc.onmessage:", event);
-        var receNum = parseInt(event.data);
-        var curTime = new Date().getTime();
-        if (isOfferer) {
-          l("offerer receive", receNum);
-          if (receNum == randNum + 1) {
-            reportResult({
-              result: "offererReceive",
-              afterStart: curTime - startTime,
-              afterOpen: curTime - openTime,
-            });
-          }
-        } else {
-          // reportResult("OK")
-          l("answerer receive", receNum);
-          sendChannel.send("" + (receNum + 1));
-          reportResult({
-            result: "answererReceive",
-            afterStart: curTime - startTime,
-            afterOpen: curTime - openTime,
-          });
-        }
-      };
-      dc.onclose = printCallback("dc.onclose");
-      dc.onerror = printCallback("dc.onerror");
-    };
+    function startWebRTC(isOfferer) {
+        l("startWebRTC isOfferer = ", isOfferer);
+        var startTime = new Date().getTime();
+        var openTime = 0;
+        pc = new RTCPeerConnection(configuration);
+        sendChannel = pc.createDataChannel("sendDataChannel");
+        sendChannel.onopen = printCallback("sendChannel.onopen");
+        sendChannel.onclose = printCallback("sendChannel.onclose");
+        sendChannel.onerror = printCallback("sendChannel.onerror");
+        sendChannel.onmessage = printCallback("sendChannel.onmessage");
+        var isFirstReceive = false;
 
-    // 当本地ICE Agent需要通过信号服务器发送信息到其他端时
-    // 会触发icecandidate事件回调
-    pc.onicecandidate = function (event) {
-      if (event.candidate) {
-        // action, content, task, another
-        sendMessage({
-          action: "chat",
-          content: { candidate: event.candidate },
-          task,
-          another,
-        });
-      }
-    };
+        var t = task;
+        setTimeout(function() {
+            if (t === window.task && !isFirstReceive)
+                reportResult({
+                    result: "Timeout",
+                    // cost: (new Date().getTime() - startTime()),
+                });
+        }, 30000);
 
-    // 如果用户是第二个进入的人，就在negotiationneeded 事件后创建sdp
-    if (isOfferer)
-      pc.onnegotiationneeded = function () {
-        // 创建本地sdp描述 SDP (Session Description Protocol) session描述协议
-        pc.createOffer()
-          .then(localDescCreated)
-          .catch(printCallback("createOffer"));
-      };
-  }
+        pc.ondatachannel = (e) => {
+            console.log("[pc.ondatachannel]", arguments);
+            window.dc = e.channel;
+            var randNum = parseInt(Math.random() * 0xffffff);
+            dc.onopen = (ev) => {
+                openTime = new Date().getTime();
+                // printCallback('dc.onopen')
+                if (isOfferer) sendChannel.send("" + randNum);
+                // reportResult({
+                //     result: "answererReceive",
+                //     afterStart: curTime - startTime,
+                //     afterOpen: curTime - openTime,
+                // })
+            };
+            dc.onmessage = function(event) {
+                if (isFirstReceive) return;
+                isFirstReceive = true;
+                l("dc.onmessage:", event);
+                var receNum = parseInt(event.data);
+                var curTime = new Date().getTime();
+                if (isOfferer) {
+                    l("offerer receive", receNum);
+                    if (receNum == randNum + 1) {
+                        reportResult({
+                            result: "offererReceive",
+                            afterStart: curTime - startTime,
+                            afterOpen: curTime - openTime,
+                        });
+                    }
+                } else {
+                    // reportResult("OK")
+                    l("answerer receive", receNum);
+                    sendChannel.send("" + (receNum + 1));
+                    reportResult({
+                        result: "answererReceive",
+                        afterStart: curTime - startTime,
+                        afterOpen: curTime - openTime,
+                    });
+                }
+            };
+            dc.onclose = printCallback("dc.onclose");
+            dc.onerror = printCallback("dc.onerror");
+        };
+
+        // 当本地ICE Agent需要通过信号服务器发送信息到其他端时
+        // 会触发icecandidate事件回调
+        pc.onicecandidate = function(event) {
+            if (event.candidate) {
+                // action, content, task, another
+                sendMessage({
+                    action: "chat",
+                    content: { candidate: event.candidate },
+                    task,
+                    another,
+                });
+            }
+        };
+
+        // 如果用户是第二个进入的人，就在negotiationneeded 事件后创建sdp
+        if (isOfferer)
+            pc.onnegotiationneeded = function() {
+                // 创建本地sdp描述 SDP (Session Description Protocol) session描述协议
+                pc.createOffer()
+                    .then(localDescCreated)
+                    .catch(printCallback("createOffer"));
+            };
+    }
 }
 
 // throw 'over'
